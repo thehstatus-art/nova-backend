a37bc83aedb12fb583de5d63bdb37e41824ccf15
thehstatus-art <thehstatus@gmail.com>
2026-03-02 22:54:07 -0500

    Fix invalid Stripe API version

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ if (!process.env.STRIPE_WEBHOOK_SECRET) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2026-01-28.clover'
+  apiVersion: '2023-10-16'
 })
 
 app.post(
